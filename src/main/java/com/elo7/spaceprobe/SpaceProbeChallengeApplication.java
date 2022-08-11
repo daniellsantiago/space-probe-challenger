231e57827e471c92b83aa0c11cac7d2b1a1d70be
Daniel Santiago <danielsantiago111@hotmail.com>
2022-08-11 00:18:38 -0300

    spring initial generated files

---
+++ src/main/java/com/elo7/spaceprobe/SpaceProbeChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.elo7.spaceprobe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceProbeChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceProbeChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elo7/spaceprobe/SpaceProbeChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elo7.spaceprobe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceProbeChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
